7bf45a2e50adc8d44a1761141077f25e38d5057c
Xarrow-008 <leox.pages@gmail.com>
2025-03-30 22:43:56 +0200

    they can talk now

--- script.js
+++ script.js
@@ -1,14 +1,52 @@
 function testFunc(){
     testCheck.hidden = !testCheck.hidden
-    testCheck.textContent=tab[getRandomInt(3)]
+    testCheck.textContent=phraseFeatures(getCriminal(allFeatures,currentFeatures))
+    return None
 }
-function getRandomInt(max) {
+function randint(max) {
     return Math.floor(Math.random() * max);
   }
-const tab=["fwaeh","night","knit"]
+
+const face = ["normal","round"]
+const eyes = ["angry","sad"]
+const hair = ["spiky","very short","no"]
+const mouth = ["smirking","big","down turned"]
+const scar = [" and a scar",""]
+const tattoo = [", plus a tattoo",""]
+const colorHair = ["black","orange","blond","red","brown"]
+const colorEyes = ["blue","green","black","brown","red","veiny"]
+const colors = [colorHair,colorEyes]
+const allFeatures = [face,eyes,hair,mouth,scar,tattoo]
+let currentFeatures = []
+
+function pushRanFeature(list,feature,colors) {
+    randFeature = randint(feature.length)
+    if (feature === hair && randFeature === 0){ //                                  If on hair and hair can have a color
+        list.push(`${colors[0][randint(colors[0].length)]} ${feature[randFeature]}`) //Push a random color and the feature description
+    } else if (feature === eyes) { //                                               Basically same for eyes (same syntax too :) )
+        list.push(`${colors[1][randint(colors[1].length)]} ${feature[randFeature]}`)
+    } else {
+        list.push(feature[randFeature])
+    }
+    return list
+}
+
+function getCriminal(allFeatures,currentFeatures){
+    currentFeatures = []
+    for (i of allFeatures) {
+        pushRanFeature(currentFeatures,i,colors)
+    }
+    return currentFeatures
+}
+
+function phraseFeatures(features) {
+    phrase = `He had a ${features[0]} face, ${features[1]} eyes and ${features[2]} hair. I think he had a ${features[3]} mouth${features[4]}${features[5]}`
+    return phrase
+}
+
 
 /*
 for (i of tab) {
-    console.log(tab[getRandomInt(3)])
+    console.log(tab[randint(3)])
 }*/
 
